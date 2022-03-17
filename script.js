2d0fd9bf6a2cd79f5678fae4e15cbea20252b6d1
yousef AlRashidi <sssaaww777@gmail.com>
2022-03-17 14:50:17 +0300

    don
    day5 cw3

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ console.log(budget > 30 && budget < 80);
 
 budget += 30;
 console.log(budget > 30 && budget < 80);
+
+// Classwork 3
+
+let grade = prompt("what is your grade?");
+
+i (grade >= 90) {
+    console. log("قد حصلت على امتياز");
+} else if (grade >= 80)  {
+ console. log("لقد حصلت على جيد جدا");
+} else if (grade >= 70) {
+    console. log(لقد حصلت على جيد); 
+} else if (grade >= 60) {
+ console. log(لقد حصلت على مقبول);
+} else if (grade >= 50) {
+ console. log(لقد حصلت على ضعيف);
+} else {
+ console. log(راسب)
+}
