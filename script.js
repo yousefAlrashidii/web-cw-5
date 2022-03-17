f59c00dbedd5114ce03011f4a9a4571d742bb5e1
yousef AlRashidi <sssaaww777@gmail.com>
2022-03-17 14:17:35 +0300

    DON
    Day5 cw2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console("يوسف");
+alert("يوسف");
 console.log(18);
 console.log(`انا في الصف حادي عشر`);
+
+// Classwork 2
+
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+
+budget += 30;
+console.log(budget > 30 && budget < 80);
